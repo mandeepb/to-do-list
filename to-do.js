c9c4ab59aa34a0e80e9d1d6b5b7299ecbfca7cba
mandeepb <kmandeepb@gmail.com>
2014-12-04 23:45:14 -0800

    add changes to code

--- to-do.js
+++ to-do.js
@@ -1,26 +1,54 @@
 // to do list
 //<li><input type="checkbox"><span>something something</span></li>
 
-function addToList(list, itemText){
-  var listItem = document.createElement('li');
-  listItem.innerText = itemText ;
+// function addToList(list, itemText){
 
-  list.appendChild(listItem);
+//   var createCheckbox = document.createElement('input');
+//   createCheckbox.type = 'checkbox';
+//   var listItem = document.createElement('li');
+//   // var createspan = document.createElement('span');
+//   // createspan.innerText = itemText ;
+// var inItemText = document.createElement('input');
+
+// inItemText.type = 'text';
+//   listItem.appendChild(inItemText);
+//   listItem.appendChild(createCheckbox);
+//   inItemText.appendChild(createspan);
+//   list.appendChild(listItem);
+// }
+
+function addText(main){
+    var input = document.getElementById('input').value;
+    //document.getElementById('do').innerHTML = input;
+    var node = document.createElement("P");
+    var createCheckbox = document.createElement('input');
+    createCheckbox.type = 'checkbox';
+
+    node.appendChild(createCheckbox);
+    var textnode = document.createTextNode(input);
+    node.appendChild(textnode);
+    var toDo = document.getElementById('do');
+    toDo.appendChild(node);
+
+
 }
 
+//var Btn = document.getElementById('btn');
+input.addEventListener('keyup', function(e) {
+//   var textnode = document.createTextNode(input);
+// //   console.log(2);
+   if(event.which == 13) {
+     console.log(1);
+    var input = document.getElementById('input').value;
 
-var inItemText = document.getElementById('inItemText');
-inItemText.focus();
-inItemText.onkeyup = function() {
 
-  if((event.which)== 13) {
-    var itemText = inItemText.value;
-
-    if (!itemText || itemText == '') {
+    if (!input || input == '') {
       return false;
     }
-    addToList(document.getElementById('list'),itemText);
+     addText(document.getElementById('main'));
   }
-};
+// //   inItemText.focus();
+// //   inItemText.select();
+});
 
 
