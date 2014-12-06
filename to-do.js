dc9e0b32fa5bf88ec346db15b533667823d9b197
mandeepb <kmandeepb@gmail.com>
2014-12-05 16:36:39 -0800

    add changes to code

--- to-do.js
+++ to-do.js
@@ -16,39 +16,52 @@
 //   inItemText.appendChild(createspan);
 //   list.appendChild(listItem);
 // }
+function updateCheckboxStatus() {
+  var cbId = this.id.replace('cb_', '');
+  var listText = document.getElementById('node_', cbId );
+
+  listText.style.textDecoration = 'line-through';
+}
 
 function addText(main){
+    totalItems++;
+
     var input = document.getElementById('input').value;
     //document.getElementById('do').innerHTML = input;
     var node = document.createElement("P");
+    node.id = 'node_'+ totalItems;
     var createCheckbox = document.createElement('input');
     createCheckbox.type = 'checkbox';
+    createCheckbox.id = 'cb_'+ totalItems;
+    createCheckbox.onclick = updateCheckboxStatus;
 
     node.appendChild(createCheckbox);
     var textnode = document.createTextNode(input);
     node.appendChild(textnode);
     var toDo = document.getElementById('do');
     toDo.appendChild(node);
-
-
+    var hr = document.createElement('hr');
+    toDo.appendChild(hr);
 }
 
-//var Btn = document.getElementById('btn');
+var totalItems = 0;
 input.addEventListener('keyup', function(e) {
 //   var textnode = document.createTextNode(input);
 // //   console.log(2);
-   if(event.which == 13) {
-     console.log(1);
+  if(event.which == 13) {
     var input = document.getElementById('input').value;
-
-
     if (!input || input == '') {
       return false;
     }
-     addText(document.getElementById('main'));
+    addText(document.getElementById('main'));
   }
-// //   inItemText.focus();
-// //   inItemText.select();
-});
 
+
+});
+$(document).ready(function() {
+  $('#main').hide();
+  $('#btn').click(function() {
+    $('#main').toggle('slow');
+  });
+});
 
